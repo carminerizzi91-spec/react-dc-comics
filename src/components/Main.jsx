9dbb52955c1154e1b449a3f498f02ee799f62ab1
carminerizzi91-spec <carminerizzi91@gmail.com>
2026-02-10 14:48:08 +0100

    add:main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,35 @@
-export default function Main(){
-    return (<main>
-        <section>
-            <h2>Contenuto</h2>
-        </section>
-    </main>)
+import comics from "../data/comics";
 
+export default function Main() {
+    return (
+        <main className="dc-main">
+
+            {/* JUMBOTRON */}
+            <div className="jumbotron"></div>
+
+
+            {/* CURRENT SERIES + CARDS */}
+            <section className="comics-section">
+                <div className="comics-container">
+
+                    <h2 className="section-title">CURRENT SERIES</h2>
+
+                    <div className="comics-grid">
+                        {comics.map((comic) => (
+                            <div className="comic-card" key={comic.id}>
+                                <img src={comic.thumb} alt={comic.title} />
+                                <h4>{comic.series}</h4>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="load-more-wrapper">
+                        <button className="load-more">LOAD MORE</button>
+                    </div>
+
+                </div>
+            </section>
+
+        </main>
+    );
 }
