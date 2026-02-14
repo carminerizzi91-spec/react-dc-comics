8e3bb14d8a6a2a69780fc50528880f960938663f
carminerizzi91-spec~ <carminerizzi91@gmail.com>
2026-02-14 18:02:15 +0100

    mod: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -47,7 +47,7 @@ export default function Footer() {
 
               <div>
                 <FooterList
-                  title={"SITE"}
+                  title={"SITES"}
                   items={sites}
                 />
               </div>
